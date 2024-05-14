b647c9de95f01350331e84eccc19615147c9c0e2
borbinha27 <141046101+borbinha27@users.noreply.github.com>
2024-05-14 18:32:26 -0300

    erro arrumado
    > era pra ser <=

--- Jogadores.java
+++ Jogadores.java
@@ -29,7 +29,7 @@ public class Jogadores {
         for (int i = 0; i < this.maoDoJogador.size(); i++) {
             soma += this.maoDoJogador.get(i);
         }
-        if(this.maoDoJogador.contains(1) && soma > 11){ //se tem Ás e seu valor total é 11 ou menos, o Ás vale 11
+        if(this.maoDoJogador.contains(1) && soma <= 11){ //se tem Ás e seu valor total é 11 ou menos, o Ás vale 11
             soma += 10; //como já foi setado o 1 caso tenha ás, aqui seto apenas 10 assim completando o 11
         }
         return soma;
